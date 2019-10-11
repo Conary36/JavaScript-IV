d236c623587469395d4d69469431872ea10cdc72
Conary36 <cbeckford144@gmail.com>
2019-10-11 17:13:38 -0400

    no errors commit of refactor

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,14 @@
 // CODE here for your Lambda Classes
+class Person{
+    constructor(attrs){
+    this.name = attrs.name;
+    this.age = attrs.age;
+    this.location = attrs.location;
+}
+speak(){
+    console.log(`Hello my name is Fred, I am from Bedrock where ${this.name} and ${this.location}`);
+}
+}
+class Instructor{
+    constructor()
+}
